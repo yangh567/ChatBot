717b6aefff4fa99f01fd9fb046cea52598e3c3c9
yangh567 <2359009s@student.gla.ac.uk>
2022-05-15 23:11:35 +0100

    some bugs fixed

--- Vosk_test.py
+++ Vosk_test.py
@@ -82,21 +82,30 @@ def return_text(model, device_info):
     end_time = 0
     user_speak_dur = []
     user_not_speaking_duration = []
+    unaccept_time = []
     user_not_speaking_but_accepting_empty_str = 0
     print("listening...")
     while True:
         # Start collecting voice
         data = stream.read(4096, exception_on_overflow=False)
-
+        # if data is there
         if len(data) > 0:
+            # if it is recognized as waveform
             if recognizer.AcceptWaveform(data):
+                # if it is not first time user speak
                 if end_time > 0:
+                    # if the first letter of spoken word is noise or empty string,
+                    # it means that user did not speak,
+                    # so calculate the duration between showing of this noise and last properly spoken word's time
+                    # if the duration is greater than 10 seconds, break
                     if len(user_speak_dur) == 0:
                         user_not_speaking_duration.append(time.time())
                         user_not_speaking_but_accepting_empty_str = user_not_speaking_duration[-1] - user_not_speaking_duration[0]
-                        if user_not_speaking_but_accepting_empty_str >= 10:
-                            break
-                        print("User empty Paused: ", user_not_speaking_but_accepting_empty_str, "Seconds")
+                        # print("User empty Paused: ", user_not_speaking_but_accepting_empty_str, "Seconds")
+                    # else, it means that user has speaked something properly
+                    # print out the word user says
+                    # update the last last properly speaked word's time
+                    # remove the time when the first letter of this word/sentence is spoken
                     else:
                         print("User Paused: ", user_speak_dur[0] - end_time, "Seconds")
                         txt = recognizer.Result()[14:-3]
@@ -105,33 +114,49 @@ def return_text(model, device_info):
                         user_speak_dur = []
                         end_time = time.time()
                         user_not_speaking_duration.append(end_time)
+
+                # if it is first time user speak
                 else:
+                    # if there is no letter in list, means the noise is accepted as waveform, just pass
                     if len(user_speak_dur) == 0:
+                        # print("empty string and 'the' accepted as waveform")
+                        # print("Accepted", recognizer.Result()[14:-3])
                         pass
-                        # print("empty string as waveform")
-                        # user_not_speaking_duration.append(time.time())
-                        # # user_not_speaking_but_accepting_empty_str = user_not_speaking_duration[-1] - \
-                        #                                             user_not_speaking_duration[0]
-                        # print("User Paused: ", user_not_speaking_but_accepting_empty_str, "Seconds")
+                    # if user spoke something, print out what user said.
+                    # update the last last properly spoken word's time
+                    # remove the time when the first letter of this word/sentence is spoken
                     else:
                         txt = recognizer.Result()[14:-3]
                         text += (txt + ".")
                         print(txt)
                         user_speak_dur = []
                         end_time = time.time()
-
-
             else:
                 dict = json.loads(recognizer.PartialResult())
                 txts = str(dict["partial"])
+                # when text is not waveform and are empty string and "the" (ambient noise)
+                # That means this is the start alpha of a word. Then, record the time when received it
                 if txts != "" and txts !="the":
                     start_time = time.time()
                     user_speak_dur.append(start_time)
                     user_not_speaking_duration = []
+                # Otherwise, user did not speak (there are noises) and thus if user have already spoken once,
+                # then record the time when noises is going in background
+                # if the noise is lasting longer than 10 seconds after last properly ended word's time
+                # break
                 else:
+                    if end_time > 0:
+                        unaccept_time.append(time.time())
+                        user_not_speaking_but_accepting_empty_str = unaccept_time[-1] - end_time
+                    # print("not accepted", txts)
                     pass
+        # if no data, keep listening
         else:
             pass
+
+        if user_not_speaking_but_accepting_empty_str >= 10:
+            print(user_not_speaking_but_accepting_empty_str)
+            break
 
         if keyboard.is_pressed("q"):
             print("Listening End\n")
